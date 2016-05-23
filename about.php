0eb9766abd3ffbc5cd0063b5cd7258de936debc8
Kevin Hinterlong <kevinhinterlong@gmail.com>
2016-05-23 04:15:10 -0500

    Front page looks nice. About page started content. Link bar is better now

--- about.php
+++ about.php
@@ -12,7 +12,7 @@
 	<script src="login.js" type="text/javascript"></script>
     </head>
 
-    <body onload="initialize();" style="background-image: url(bg.png)">
+    <body onload="initialize();">
 	<div class="header">
 	    <h1>Welcome to <?php echo $config["websiteName"]; ?>!</h1>
 	    <div id="login">
@@ -47,65 +47,33 @@
 	<div class="main">
 	    <div id="intro" class="content-pane">
 		<div class="description left">
-		    <p>Do you ever feel like keeping track of your family is impossible?</p>
-		    <p>Is it getting too difficult to keep track of what you need to do?</p>
-		    <p>With <?php echo $config["websiteName"]; ?>, you'll be able to do all that and more!</p>
+		    <h1>Who are we?</h1>
+		    <p><?php echo $config["websiteName"]; ?> is a growing company started at IMSA.</p>
+		    <p>Our goal is to create a user friendly way of assigning tasks and keeping track of tasks.</p>
 		</div>
     
 		<div class="feature right">
 		    <div class="feature-image">
-			<img src="https://pixabay.com/static/uploads/photo/2013/07/12/18/22/checklist-153371_960_720.png" alt="checklist">
+			<img src="https://pixabay.com/static/uploads/photo/2015/01/08/18/25/startup-593327_960_720.jpg" style="height:20%;" alt="computer">
 		    </div>
 		    <div class="feature-description">
-			<h2>"I use this product all the time. It's fantastic!" - Kevin Hinterlong</h2>
+			<h2>Description</h2>
 		    </div>
 		</div>
     
 	    </div>
 
-
-	    <div id="moreDetails" class="content-pane">
-		<div class="description right">
-		    <p><?php echo $config["websiteName"]; ?>'s original purpose was to present chores and work in a task based system.</p>
-		    <p>With our active development team, you'll soon be able to create a reward system for the tasks you assign.</p>
-		    <p><?php echo $config["websiteName"]; ?> can be used by parents, grade schools, even development teams!</p>
-		</div>
-    
-		<div class="feature left">
-		    <div class="feature-image">
-			<img src="https://pixabay.com/static/uploads/photo/2013/07/12/19/04/swiss-army-knife-154314_960_720.png" alt="versatility">
-		    </div>
-		    <div class="feature-description">
-			<h2>"This app has so many uses it's insane!" - John Valin</h2>
-		    </div>
-		</div>
-    
-	    </div>
-
-
-	    <div id="more" class="content-pane">
-		<div class="description left">
-		    <p>Our product will be completely free to host yourself, but we also plan to offer low-cost hosting.</p>
-		    <p>Please feel free to contact our development team if you have any issues or suggestions.</p>
-		</div>
-    
-		<div class="feature right">
-		    <div class="feature-image">
-			<img src="https://pixabay.com/static/uploads/photo/2016/03/31/20/40/arrow-1295953_960_720.png" alt="graph">
-		    </div>
-		    <div class="feature-description">
-			<h2>"I feel so much more productive using this!" - Daniel Martin</h2>
-		    </div>
-		</div>
-    
-	    </div>
 
 	    <div class="disclaimer">
 		<div class="warning-picture">
 		    <i class="material-icons">warning</i>
 		</div>
-		<div class="warning-preview"> Disclaimer!</div>
-		<div class="warning-content"> Please note that this project was made for a school project and the above statements do not represent me or any company.</p> </div>
+		<div class="warning-preview">
+		    Disclaimer!
+		</div>
+		<div class="warning-content">
+		    Please note that this project was made for a school project and the above statements do not represent me or any company.</p>
+		</div>
 	    </div>
 
 	</div>
@@ -135,6 +103,5 @@
 	    });
 
 	</script>
-
     </body>
 </html>
